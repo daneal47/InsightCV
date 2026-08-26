1474242185be401741e1a6b9af28e553e66a939f
daneal47 <152391334+daneal47@users.noreply.github.com>
2026-08-27 01:34:30 +0300

    Update App.py

--- App/App.py
+++ App/App.py
@@ -2,6 +2,14 @@
 
 ###### Packages Used ######
 import streamlit as st # core package used in this project
+import nltk
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('universal_tagset')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
 import pandas as pd
 import base64, random
 import time,datetime
